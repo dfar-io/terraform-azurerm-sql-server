8016106dd6efcbfb3fb0073034bb4bc9c2b592dd
Dave Farinelli <d@dfar.io>
2020-11-19 10:23:20 -0500

    Fixed interpolation issues

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "sqlserver_name" {
-  value = "${azurerm_sql_server.sqlserver.name}"
+  value = azurerm_sql_server.sqlserver.name
 }
 
 output "sqlserver_administrator_login" {
   description = "Username for SQL Server admin user."
-  value = "${azurerm_sql_server.sqlserver.administrator_login}"
+  value = azurerm_sql_server.sqlserver.administrator_login
 }
 
 output "sqlserver_administrator_login_password" {
   description = "Password for SQL Server admin user."
-  value = "${azurerm_sql_server.sqlserver.administrator_login_password}"
+  value = azurerm_sql_server.sqlserver.administrator_login_password
 }
